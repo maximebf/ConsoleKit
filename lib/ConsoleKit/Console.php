57d3839f0dc30df62318994f79cc84431b09d35f
maximebf <maxime.bouroumeau@gmail.com>
2012-08-18 10:30:47 +0200

    updated copyright notice

--- lib/ConsoleKit/Console.php
+++ lib/ConsoleKit/Console.php
@@ -1,20 +1,11 @@
 <?php
-/**
- * ConsoleKit
- * Copyright (c) 2012 Maxime Bouroumeau-Fuseau
+/*
+ * This file is part of the ConsoleKit package.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * (c) 2012 Maxime Bouroumeau-Fuseau
  *
- * @author Maxime Bouroumeau-Fuseau
- * @copyright 2012 (c) Maxime Bouroumeau-Fuseau
- * @license http://www.opensource.org/licenses/mit-license.php
- * @link http://github.com/maximebf/ConsoleKit
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
  
 namespace ConsoleKit;
